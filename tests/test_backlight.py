6514a799b7104bb6edb320796a2a9eb1fe744e22
Linus Groh <mail@linusgroh.de>
2019-06-30 23:03:58 +0100

    Format test_backlight.py with black

--- tests/test_backlight.py
+++ tests/test_backlight.py
@@ -3,11 +3,13 @@ import pytest
 from rpi_backlight import Backlight
 from rpi_backlight.utils import FakeBacklightSysfs
 
+
 def test_get_brightness() -> None:
     with FakeBacklightSysfs() as backlight_sysfs:
         backlight = Backlight(backlight_sysfs_path=backlight_sysfs.path)
 
         assert backlight.brightness == 100
+
 
 def test_set_brightness() -> None:
     with FakeBacklightSysfs() as backlight_sysfs:
@@ -31,11 +33,13 @@ def test_set_brightness() -> None:
         with pytest.raises(ValueError):
             backlight.brightness = -1
 
+
 def test_get_power() -> None:
     with FakeBacklightSysfs() as backlight_sysfs:
         backlight = Backlight(backlight_sysfs_path=backlight_sysfs.path)
 
         assert backlight.power is True
+
 
 def test_set_power() -> None:
     with FakeBacklightSysfs() as backlight_sysfs:
