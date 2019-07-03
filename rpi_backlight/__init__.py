42f3aa527f64a0996f52059894751631c812d73f
Linus Groh <mail@linusgroh.de>
2019-07-03 20:21:25 +0100

    Bump version to 2.0.0b7

--- rpi_backlight/__init__.py
+++ rpi_backlight/__init__.py
@@ -5,7 +5,7 @@ from tempfile import gettempdir
 from typing import Any, Callable, Iterator, Union
 
 __author__ = "Linus Groh"
-__version__ = "2.0.0b6"
+__version__ = "2.0.0b7"
 __all__ = ["Backlight"]
 
 _BACKLIGHT_SYSFS_PATH = "/sys/class/backlight/rpi_backlight/"
