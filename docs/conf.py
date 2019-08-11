ca7914788892ac0f369cff3813d6f1c46cc54113
Linus Groh <mail@linusgroh.de>
2019-08-11 16:57:24 +0100

    Add master_doc to sphinx config

--- docs/conf.py
+++ docs/conf.py
@@ -39,6 +39,8 @@ extensions = ["sphinx.ext.autodoc"]
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
+master_doc = "index"
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
