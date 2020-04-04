7379c23fea8e14f2f660db063a455775173e82db
p1r473 <subwayjared@gmail.com>
2020-04-04 17:01:47 -0400

    Update cli.py

--- rpi_backlight/cli.py
+++ rpi_backlight/cli.py
@@ -50,7 +50,7 @@ def _create_argument_parser():
     parser.add_argument(
         "-B",
         "--board-type",
-        default="tinker-board",
+        default="raspberry-pi",
         choices=BOARD_TYPES.keys(),
         help="board type",
     )
