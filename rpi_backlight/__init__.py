da83c350bc720a0e64db19b946e78cf8499a220e
p1r473 <subwayjared@gmail.com>
2021-06-09 11:24:58 -0400

    Update __init__.py

--- rpi_backlight/__init__.py
+++ rpi_backlight/__init__.py
@@ -21,13 +21,13 @@ class BoardType(Enum):
     #: Tinker Board
     TINKER_BOARD = 2
     #: Tinker Board 2
-    TINKER_BOARD2 = 3
+    TINKER_BOARD_2 = 3
 
 
 _BACKLIGHT_SYSFS_PATHS = {
     BoardType.RASPBERRY_PI: "/sys/class/backlight/rpi_backlight/",
     BoardType.TINKER_BOARD: "/sys/devices/platform/ff150000.i2c/i2c-3/3-0045/",
-    BoardType.TINKER_BOARD2: "/sys/devices/platform/ff3e0000.i2c/i2c-8/8-0045/",
+    BoardType.TINKER_BOARD_2: "/sys/devices/platform/ff3e0000.i2c/i2c-8/8-0045/",
 }
 _EMULATOR_SYSFS_TMP_FILE_PATH = Path(gettempdir()) / "rpi-backlight-emulator.sysfs"
 _EMULATOR_MAGIC_STRING = ":emulator:"
@@ -74,7 +74,7 @@ class Backlight:
             self._max_brightness = self._get_value("max_brightness")  # 255
         elif (
             self._board_type == BoardType.TINKER_BOARD
-            or self._board_type == BoardType.TINKER_BOARD2
+            or self._board_type == BoardType.TINKER_BOARD_2
         ):
             self._max_brightness = 255
 
@@ -163,7 +163,7 @@ class Backlight:
             return self._normalize_brightness(self._get_value("actual_brightness"))
         elif (
             self._board_type == BoardType.TINKER_BOARD
-            or self._board_type == BoardType.TINKER_BOARD2
+            or self._board_type == BoardType.TINKER_BOARD_2
         ):
             return self._normalize_brightness(self._get_value("tinker_mcu_bl"))
         else:
@@ -189,7 +189,7 @@ class Backlight:
                     )
                 elif (
                     self._board_type == BoardType.TINKER_BOARD
-                    or self._board_type == BoardType.TINKER_BOARD2
+                    or self._board_type == BoardType.TINKER_BOARD_2
                 ):
                     self._set_value(
                         "tinker_mcu_bl", self._denormalize_brightness(current_value)
@@ -202,7 +202,7 @@ class Backlight:
                 self._set_value("brightness", self._denormalize_brightness(value))
             elif (
                 self._board_type == BoardType.TINKER_BOARD
-                or self._board_type == BoardType.TINKER_BOARD2
+                or self._board_type == BoardType.TINKER_BOARD_2
             ):
                 self._set_value("tinker_mcu_bl", self._denormalize_brightness(value))
             else:
@@ -226,7 +226,7 @@ class Backlight:
             return not self._get_value("bl_power")
         elif (
             self._board_type == BoardType.TINKER_BOARD
-            or self._board_type == BoardType.TINKER_BOARD2
+            or self._board_type == BoardType.TINKER_BOARD_2
         ):
             return bool(self._get_value("tinker_mcu_bl"))
         else:
@@ -242,7 +242,7 @@ class Backlight:
             self._set_value("bl_power", int(not on))
         elif (
             self._board_type == BoardType.TINKER_BOARD
-            or self._board_type == BoardType.TINKER_BOARD2
+            or self._board_type == BoardType.TINKER_BOARD_2
         ):
             self._set_value("tinker_mcu_bl", 255 if on else 0)
         else:
