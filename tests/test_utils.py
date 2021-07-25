0252acd2a38d8e0167562bda05c7c65153d68970
Linus Groh <mail@linusgroh.de>
2021-07-25 12:21:56 +0100

    Add tests for utils.detect_board_type()

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,9 @@
-from rpi_backlight.utils import FakeBacklightSysfs
+from pathlib import Path
+
+import pytest
+
+from rpi_backlight import BoardType
+from rpi_backlight.utils import FakeBacklightSysfs, detect_board_type
 
 
 def test_fake_sysfs_backlight() -> None:
@@ -13,3 +18,25 @@ def test_fake_sysfs_backlight() -> None:
             assert (backlight_sysfs.path / filename).exists() is True
 
     assert backlight_sysfs.path.exists() is False
+
+
+@pytest.mark.parametrize(
+    "model,board_type",
+    [
+        ("Raspberry Pi 3 Model B Rev 1.2", BoardType.RASPBERRY_PI),
+        ("Raspberry Pi 42", BoardType.RASPBERRY_PI),
+        ("Raspberry Pi", BoardType.RASPBERRY_PI),
+        ("ASUS Tinker Board 2", BoardType.TINKER_BOARD_2),
+        ("ASUS Tinker Board 2S", BoardType.TINKER_BOARD_2),
+        ("Tinker Board 2", BoardType.TINKER_BOARD_2),
+        ("Tinker Board 2S", BoardType.TINKER_BOARD_2),
+        ("Rockchip RK3288 Asus Tinker Board", BoardType.TINKER_BOARD),
+        ("Rockchip RK3288 Asus Tinker Board S", BoardType.TINKER_BOARD),
+        ("Tinker Board", BoardType.TINKER_BOARD),
+        ("Something else", None),
+    ],
+)
+def test_detect_board_type(monkeypatch, model, board_type) -> None:
+    monkeypatch.setattr(Path, "read_text", lambda self: model)
+
+    assert detect_board_type() == board_type
